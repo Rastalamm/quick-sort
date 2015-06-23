e5c9d10ab2e73c224a17368512dc5d373efa8a86
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-22 16:06:49 -1000

    starting to build tests:

--- quicksort.js
+++ quicksort.js
@@ -1,13 +1,27 @@
-var initialArr = [10, 7, 6, 19, 21, 4]
-
 var quickSort = function(arr){
-  pivot = arr[0];
-  left = [];
-  right =[];
+  var pivot = arr.splice(0,1);
+  var left = [];
+  var right = [];
 
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] < pivot){
+      left.push(arr[i]);
+    }
+    else{
+      right.push(arr[i]);
+    }
+  }
 
+  if(left.length > 1){
+      left = quickSort(left);
+  }
 
+  if(right.length > 1){
+      right =   quickSort(right);
+  }
+
+  return left.concat(pivot, right);
 
 }
 
-quickSort(initialArr);
+module.exports = quickSort;
